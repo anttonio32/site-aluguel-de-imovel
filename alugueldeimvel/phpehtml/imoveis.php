52638c7a1816bda7f3bbe66bbf9ae2788bff0d81
EricGustavo <202015210050@ifba.edu.br>
2023-05-17 20:59:57 -0300

    Adicionando os arrays

--- alugueldeimvel/phpehtml/imoveis.php
+++ alugueldeimvel/phpehtml/imoveis.php
@@ -1,2 +1,33 @@
 <?php
-    $imovel = [];
+    $imovel = [
+    [
+        "nome" => "Apartamento",
+        "foto" => " ",
+        "cidade" => "São Paulo",
+        "estado" => "SP",
+        "complemento" => "Centro da cidade",
+        "quartos" => "2 suítes",
+        "valor" => 950,
+     ],
+[
+    "nome" => "Chalé",
+    "foto" => " ",
+    "cidade" => "Lençóis",
+    "estado" => "BA",
+    "complemento" => "Contato com a natureza",
+    "quartos" => "3 beliches",
+    "valor" => 450,
+
+    ],
+[
+    "nome" => "Casa de Praia",
+    "foto" => " ",
+    "cidade" => "Aracaju",
+    "estado" => "SE",
+    "complemento" => "Orla de Praia",
+    "quartos" => "5 suítes",
+    "valor" => 2500,
+ ]
+
+
+];
