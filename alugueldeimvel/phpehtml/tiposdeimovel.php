a1671d1ded57f1c669dfb4a8beea2a78b214ff41
Eric Ifba <202015210050@ifba.edu.br>
2023-05-23 15:56:20 -0300

    Adicionando css

--- alugueldeimvel/phpehtml/tiposdeimovel.php
+++ alugueldeimvel/phpehtml/tiposdeimovel.php
@@ -15,15 +15,12 @@
 <html>
 <head>
 	<title>tipos de Imoveis</title>
-	<link rel="stylesheet" type="text/css" href="style03.css">
+	<link rel="stylesheet" type="text/css" href="../css/style03.css">
 </head>
 <body>
     <header>
         <h1>Listagem dos imoveis</h1>
     </header>
-	<div class="container-del">
-		
-		
 		<div class="cad">
 			<?php 
 				$tipos = $imovel_detalhe[$loc];
@@ -41,12 +38,17 @@
 					$valor = $valor * ($check * 3);
 				}
 			?>
-			<img src=" <?= $tipos["foto"] ?> " alt=" <?= $tipos["nome"]?> ">
-			<h3> <?=$tipos["nome"]?> </h3>
-			<p> <?=$tipos["descricao"]?> </p>
-			<p>Valor: <?= $valor ?></p>
-			<a href="../index.php" class="link">Voltar</a>
+			<div class="fotoCad">
+				<img src=" <?= $tipos["foto"] ?> " alt=" <?= $tipos["nome"]?> ">
+			</div>
+			<div class="desc">
+				<h3> <?=$tipos["nome"]?> </h3>
+				<p> <?=$tipos["descricao"]?> </p>
+				<p>Valor: <?= $valor ?></p>
+				<a href="../index.php" class="link">
+					<button>Voltar</button>
+				</a>
+			</div>
 		</div>
-	</div>
 </body>
 </html>
